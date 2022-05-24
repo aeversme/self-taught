4700b845fc09dc536c42f45f94a969cf4fd602d8
Alex <aeversme@gmail.com>
2022-05-24 14:49:35 -0400

    add bubble sort

---
+++ self-taught-computer-scientist/sorting/bubble_sort.py
@@ -0,0 +1,47 @@
+# Bubble Sort
+from random import randint
+import datetime
+
+
+number_list = [8, 4, 9, 6, 5, 1, 3, 7, 2]
+bigger_list = [randint(0, 100000) for _ in range(10000)]
+
+
+def bubble_sort(a_list):
+    swap_count = -1
+    while swap_count != 0:
+        swap_count = 0
+        for i in range(len(a_list) - 1):
+            if a_list[i] > a_list[i + 1]:
+                a_list[i], a_list[i + 1] = a_list[i + 1], a_list[i]
+                swap_count += 1
+    return a_list
+
+
+def bubble_sort_book(a_list):
+    list_length = len(a_list) - 1
+    for i in range(list_length):
+        no_swaps = True
+        for j in range(list_length):
+            if a_list[j] > a_list[j + 1]:
+                a_list[j], a_list[j + 1] = a_list[j + 1], a_list[j]
+                no_swaps = False
+        if no_swaps:
+            return a_list
+    return a_list
+
+
+def compare_sorts(input_list):
+    input_list_copy = input_list.copy()
+    first_start = datetime.datetime.utcnow()
+    bubble_sort(input_list)
+    first_end = datetime.datetime.now().utcnow()
+    print(f"First sort: {(first_end - first_start).total_seconds()} seconds")
+    second_start = datetime.datetime.utcnow()
+    bubble_sort_book(input_list_copy)
+    second_end = datetime.datetime.utcnow()
+    print(f"Second sort: {(second_end - second_start).total_seconds()} seconds")
+    return None
+
+
+compare_sorts(bigger_list)
